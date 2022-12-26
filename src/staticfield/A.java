360dd50db08cc6e1f263e454ae513ed61dbc1920
Soyoung <sy.park.fe@gmail.com>
2022-12-26 10:14:01 +0900

    아무내용

---
+++ src/staticfield/A.java
@@ -0,0 +1,36 @@
+package staticfield;
+
+public class A {
+	int m = 3;
+	static int n = 5;
+
+	public static void main(String[] args) {
+		A a1 = new A();
+		A a2 = new A();
+		
+		a1.m = 5;
+		a2.m = 6;
+		System.out.println(a1.m);
+		System.out.println(a2.m);
+		
+		a1.n = 7;
+		a2.n = 8;
+		System.out.println();
+		System.out.println(A.n);
+		
+		
+		A.n = 9;
+		System.out.println(a1.n);
+		System.out.println(a2.n);
+		System.out.println(A.n);
+		
+		A.n = 9;
+		System.out.println(a1.n);
+		System.out.println(a2.n);
+		System.out.println(A.n);
+				
+		
+
+	}
+
+}
